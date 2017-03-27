b5c04ef20aef551a61bb5229f1f03f76da83d128
smoore2386 <smoore2386@gmail.com>
2017-03-27 16:12:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// Copy paste example of usage for gpio 
+
+var Gpio = require('onoff').Gpio,
+    led = new Gpio(14, 'out'),
+    button = new Gpio(4, 'in', 'both');
+
+button.watch(function (err, value) {
+    if (err) {
+	throw err;
+    }
+
+    led.writeSync(value);
+});
+
+process.on('SIGINT', function () {
+    led.unexport();
+    button.unexport();
+});
